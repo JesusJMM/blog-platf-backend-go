27f56a6328bd4d67f6e0a43760e52484b966b81e
jsus <jsusmarcano.jm@gmail.com>
2022-07-11 15:45:42 -0400

    refactor tests

--- api/articles/controllers_test.go
+++ api/articles/controllers_test.go
@@ -3,6 +3,7 @@ package articles
 import (
 	"context"
 	"encoding/json"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -42,6 +43,15 @@ var testArticleData = []map[string]interface{}{
 
 type commonBodyType map[string][]map[string]interface{}
 
+// Set up a new router and make a request
+func makeRequest(db ksql.Mock, method, url string, body io.Reader) (*httptest.ResponseRecorder){
+  r := setupTestingRouter(db)
+  w := httptest.NewRecorder()
+  req, _ := http.NewRequest(method, url, body)
+  r.ServeHTTP(w, req)
+  return w
+}
+
 // test the 'all' controller
 func Test_AllController(t *testing.T) {
 	t.Run("Should returns the entire article set", func(t *testing.T) {
@@ -51,11 +61,7 @@ func Test_AllController(t *testing.T) {
 				return nil
 			},
 		}
-		r := setupTestingRouter(mockDB)
-
-		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("GET", "/all", nil)
-		r.ServeHTTP(w, req)
+    w := makeRequest(mockDB, "GET", "/all", nil)
 
 		var body = make(commonBodyType, 3)
 
@@ -72,12 +78,7 @@ func Test_AllController(t *testing.T) {
 				return errors.New("Something went grown")
 			},
 		}
-		r := setupTestingRouter(mockDB)
-
-		w := httptest.NewRecorder()
-		req, _ := http.NewRequest("GET", "/all", nil)
-		r.ServeHTTP(w, req)
-
+    w := makeRequest(mockDB, "GET", "/all", nil)
 		assert.Equal(t, 500, w.Code)
 	})
 }
@@ -85,10 +86,7 @@ func Test_AllController(t *testing.T) {
 // Test the 'Paginated' controller
 func Test_PaginatedController(t *testing.T){
   t.Run("Should return 400 if pass invaild page param", func(t *testing.T) {
-    r := setupTestingRouter(ksql.Mock{})
-    w := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/paginated?page=asdf", nil)
-    r.ServeHTTP(w, req)
+    w := makeRequest(ksql.Mock{}, "GET", "/paginated?page=asdf", nil)
 
     assert.Equal(t, 400, w.Code)
   })
@@ -99,11 +97,7 @@ func Test_PaginatedController(t *testing.T){
         return nil
       },
     }
-    r := setupTestingRouter(mockDB)
-    w := httptest.NewRecorder()
-    req, _ := http.NewRequest("GET", "/paginated?page=asdf", nil)
-    r.ServeHTTP(w, req)
-
+    w := makeRequest(mockDB, "GET", "/paginated?page=1", nil)
     assert.Equal(t, 200, w.Code)
   })
 }
