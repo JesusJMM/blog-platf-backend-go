0ad71ec3a58da730529cad8ebe34e19e8f1f3053
jsus <jsusmarcano.jm@gmail.com>
2022-07-11 15:15:37 -0400

    add test

--- api/articles/controllers_test.go
+++ api/articles/controllers_test.go
@@ -9,13 +9,14 @@ import (
 
 	"github.com/JesusJMM/blog-plat-go/postgres/repos/articles"
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/vingarcia/ksql"
 	"github.com/vingarcia/ksql/ksqltest"
 )
 
-func setupTestingRouter(db ksql.Provider, ctx context.Context, articleRepo articles.ArticleRepository) *gin.Engine {
-	controllers := New(db, ctx, articleRepo)
+func setupTestingRouter(db ksql.Provider) *gin.Engine {
+	controllers := New(db, context.Background(), articles.NewMockedArticleRepo())
 	r := gin.Default()
 	r.GET("/all", controllers.All())
 	r.GET("/paginated", controllers.Paginated())
@@ -26,42 +27,57 @@ func setupTestingRouter(db ksql.Provider, ctx context.Context, articleRepo artic
 
 var testArticleData = []map[string]interface{}{
 	{
-		"article_id":    0,
-		"title": "Test Article",
-		"slug":  "Article Description",
-    "user_id": 3,
+		"article_id": 0,
+		"title":      "Test Article",
+		"slug":       "Article Description",
+		"user_id":    3,
 	},
 	{
-		"article_id":    1,
-		"title": "Test Article",
-		"slug":  "Article Description",
-    "user_id": 3,
+		"article_id": 1,
+		"title":      "Test Article",
+		"slug":       "Article Description",
+		"user_id":    3,
 	},
 }
+
+type commonBodyType map[string][]map[string]interface{}
 
 // test the 'all' controller
 func Test_AllController(t *testing.T) {
 	t.Run("Should returns the entire article set", func(t *testing.T) {
 		mockDB := ksql.Mock{
 			QueryFn: func(ctx context.Context, record interface{}, query string, params ...interface{}) error {
-        ksqltest.FillSliceWith(record, testArticleData)
+				ksqltest.FillSliceWith(record, testArticleData)
 				return nil
 			},
 		}
-		r := setupTestingRouter(mockDB, context.Background(), articles.NewMockedArticleRepo())
-
+		r := setupTestingRouter(mockDB)
 
 		w := httptest.NewRecorder()
 		req, _ := http.NewRequest("GET", "/all", nil)
 		r.ServeHTTP(w, req)
 
-    var body = make(map[string][]map[string]interface{}, 3)
+		var body = make(commonBodyType, 3)
 
-    err := json.Unmarshal(w.Body.Bytes(), &body)
-    if err != nil {
-      t.Errorf("Fail to decode the body response: %v", err)
-    }
+		err := json.Unmarshal(w.Body.Bytes(), &body)
+		if err != nil {
+			t.Fatalf("Fail to decode the body response: %v", err)
+		}
 		assert.Equal(t, 200, w.Code)
-    assert.NotEqual(t, len(body["articles"]), len(testArticleData))
+		assert.Equal(t, len(body["articles"]), len(testArticleData))
+	})
+	t.Run("Should Return 500 if db returns error", func(t *testing.T) {
+		mockDB := ksql.Mock{
+			QueryFn: func(ctx context.Context, record interface{}, query string, params ...interface{}) error {
+				return errors.New("Something went grown")
+			},
+		}
+		r := setupTestingRouter(mockDB)
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/all", nil)
+		r.ServeHTTP(w, req)
+
+		assert.Equal(t, 500, w.Code)
 	})
 }
