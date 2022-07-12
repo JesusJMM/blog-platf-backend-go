d3205e13bc8ae3c98342b80e95054fe5328c2d1d
jsus <jsusmarcano.jm@gmail.com>
2022-07-12 12:50:49 -0400

    add warning

--- postgres/conn.go
+++ postgres/conn.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/vingarcia/ksql"
@@ -13,7 +14,9 @@ import (
 // uses POSTGRES_URL enviroment variable
 func New() (*ksql.DB, error) {
 	ctx := context.Background()
-
+  if os.Getenv("POSTGRES_URL") == "" {
+    log.Fatal("POSTGRES_URL enviroment variable is not set.")    
+  }
 	db, err := kpgx.New(ctx, os.Getenv("POSTGRES_URL"), ksql.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("Error opening database: %w", err)
